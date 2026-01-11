4cf57550875bd90a6888b27f5277d18d495b2c89
Andrew Yamamoto <andrewy@maxknowledge.com>
2026-01-10 18:46:23 -0800

    set up root page'

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from nicegui import ui
 import os
 
-ui.label('Hello NiceGUI!')
+@ui.page('/')
+def main_page():
+    ui.label('Hello NiceGUI!')
 
 ui.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
